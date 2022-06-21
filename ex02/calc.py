76158c13ccf0606ea9b18e9a7db52b5769980ae1
yakame <c0a211288b@edu.teu.ac.jp>
2022-06-21 14:47:25 +0900

    5made

---
+++ ex02/calc.py
@@ -0,0 +1,37 @@
+from logging import root
+import tkinter as tk
+import tkinter.messagebox as tkm
+
+def button_click(event):
+    btn = event.widget
+    num = btn["text"]
+    #tkm.showinfo("",f"{num}のボタンがクリックされました")
+    entry.insert(tk.END,num)
+    
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.geometry("300x500")
+    root.title("電卓")
+    
+    entry = tk.Entry(root,
+                     justify="right",
+                     width=10,
+                     font=("Times New Roman",40)
+                     )
+    entry.grid(row=0,column=0,columnspan=3)
+    
+    r,c=1,0
+    for num in range(9,-1,-1):
+        btn = tk.Button(root,
+                        text=f"{num}",
+                        width=4,
+                        height=2,
+                        font=("Times New Roman",30)
+                        )
+        btn.bind("<1>",button_click)
+        btn.grid(row=r,column=c)
+        c+=1
+        if (num-1)%3==0:
+            r+=1
+            c=0  
+    root.mainloop()
