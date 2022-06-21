9033933b507ed2ac119eda270ef406c1ab29029c
yakame <c0a211288b@edu.teu.ac.jp>
2022-06-21 16:46:36 +0900

    fin

--- ex02/calc.py
+++ ex02/calc.py
@@ -2,22 +2,32 @@ from logging import root
 import tkinter as tk
 import tkinter.messagebox as tkm
 
-
+ 
 def button_click(event):
     btn = event.widget
     num = btn["text"]#クリックされたボタンの文字
+    
     if num == "=":
         eqn = entry.get()
         res = eval(eqn)
         entry.delete(0,tk.END)
         entry.insert(tk.END,res)
+    elif num == "C":
+        entry.delete(0,tk.END)
+        
+    elif num == "2進数":
+        eq = int(entry.get())
+        entry.delete(0,tk.END)
+        a = format(eq,"b")
+        entry.insert(tk.END,a)
+    
     else:  
     #tkm.showinfo("",f"{num}のボタンがクリックされました")
         entry.insert(tk.END,num)
     
 if __name__ == "__main__":
     root = tk.Tk()
-    root.geometry("300x600")
+    root.geometry("500x700")
     root.title("電卓")
     
     entry = tk.Entry(root,
@@ -28,17 +38,23 @@ if __name__ == "__main__":
     entry.grid(row=0,column=0,columnspan=3)
     
     r,c=1,0
-    for i,num in enumerate([i for i in range(9,-1,-1)]+["+"]+["="]):
+    for i,num in enumerate(["2進数","","","C",
+                            9,8,7,"/",
+                            6,5,4,"*",
+                            3,2,1,"-",
+                            0,"","=","+",]
+                           ):
+        
         btn = tk.Button(root,
                         text=f"{num}",
-                        width=4,
-                        height=2,
+                        width=5,
+                        height=1,
                         font=("Times New Roman",30)
                         )
         btn.bind("<1>",button_click)
         btn.grid(row=r,column=c)
         c+=1
-        if (i+1)%3==0:
+        if (i+1)%4==0:
             r+=1
-            c=0  
+            c=0
     root.mainloop()
