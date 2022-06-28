af81feb255aee4150ac6614e02a9fee0be9f0649
yakame <c0a211288b@edu.teu.ac.jp>
2022-06-28 14:55:45 +0900

    7

---
+++ ex03/maze.py
@@ -0,0 +1,46 @@
+import tkinter as tk
+
+def key_down(event):
+    global key
+    key = event.keysym
+    #print(f"{key}が押されました")
+
+
+def key_up(event):
+    global key
+    key = ""
+
+
+def main_proc():
+    global cx, cy
+    delta = { 
+        ""     : [0,   0],
+        "Up"   : [0, -20],
+        "Down" : [0, +20],
+        "Left" : [-20, 0],
+        "Right": [+20, 0]      
+    }
+    cx, cy = cx+delta[key][0], cy+delta[key][1]
+    canvas.coords("tori", cx, cy)
+    root.after(50, main_proc)
+    
+    
+    
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.title("迷えるこうかとん")
+    
+    canvas = tk.Canvas(root, width=1500, height=900, bg="black")
+    canvas.pack()
+    
+    tori = tk.PhotoImage(file="fig/2.png")
+    cx,cy = 300, 400
+    canvas.create_image(cx, cy, image=tori, tag="tori")
+    
+    key = ""
+    root.bind("<KeyPress>",key_down)
+    root.bind("<KeyRelease>",key_up)
+    
+    main_proc()
+    root.mainloop()
+
